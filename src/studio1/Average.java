7b305a0fca41464c3eef9177b73da00cfffd5d6d
HamHamHa <hanielquartz@gmail.com>
2025-09-03 13:59:25 -0500

    done

---
+++ src/studio1/Average.java
@@ -0,0 +1,15 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("The first of two integers to be averaged");
+        double n1 = in.nextInt();
+        System.out.println("The second of two integers to be averaged");
+        double n2 = in.nextInt();
+        double average = ((n1 + n2)/2);
+        System.out.print("Average of " + n1 +  " and " + n2 + " is " + average + ".");
+    }
+}
